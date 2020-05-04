aa78d4e89f6a99bc07574c37c214ceee45b586a4
ZushiBot <ZushiBot@users.noreply.github.com>
2020-05-04 23:25:52 +0600

    Updates

---
+++ precache-manifest.b3580e41d89e37937efe1bfcb3fda516.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e2ed506926b321b4a4e87b650a46ab",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "b8f8529600f7a259ad4c",
+    "url": "/facerecognition/static/css/2.22913aca.chunk.css"
+  },
+  {
+    "revision": "f2ad5be4b338f63614ea",
+    "url": "/facerecognition/static/css/main.eb3ef7ad.chunk.css"
+  },
+  {
+    "revision": "b8f8529600f7a259ad4c",
+    "url": "/facerecognition/static/js/2.99d03d10.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/facerecognition/static/js/2.99d03d10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2ad5be4b338f63614ea",
+    "url": "/facerecognition/static/js/main.dae1ab94.chunk.js"
+  },
+  {
+    "revision": "7747f7a09a564ebd257a",
+    "url": "/facerecognition/static/js/runtime-main.8421dbec.js"
+  }
+]);
